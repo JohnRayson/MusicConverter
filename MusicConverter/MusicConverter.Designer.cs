b3a6cd7bd14688cad08134bc783d7481c7b24bf2
John Rayson <j.d.rayson@googlemail.com>
2015-07-07 23:03:37 +0100

    Version 0.00002

---
+++ MusicConverter/MusicConverter.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace MusicConverter
+{
+    partial class MusicConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenSourceFolderBtn = new System.Windows.Forms.Button();
+            this.OpenDestinationFolderBtn = new System.Windows.Forms.Button();
+            this.SourceFolderLbl = new System.Windows.Forms.Label();
+            this.DestinationFolderLbl = new System.Windows.Forms.Label();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OpenSourceFolderBtn
+            // 
+            this.OpenSourceFolderBtn.Location = new System.Drawing.Point(12, 12);
+            this.OpenSourceFolderBtn.Name = "OpenSourceFolderBtn";
+            this.OpenSourceFolderBtn.Size = new System.Drawing.Size(154, 40);
+            this.OpenSourceFolderBtn.TabIndex = 0;
+            this.OpenSourceFolderBtn.Text = "Source Folder";
+            this.OpenSourceFolderBtn.UseVisualStyleBackColor = true;
+            this.OpenSourceFolderBtn.Click += new System.EventHandler(this.OpenSourceFolderBtn_Click);
+            // 
+            // OpenDestinationFolderBtn
+            // 
+            this.OpenDestinationFolderBtn.Location = new System.Drawing.Point(13, 58);
+            this.OpenDestinationFolderBtn.Name = "OpenDestinationFolderBtn";
+            this.OpenDestinationFolderBtn.Size = new System.Drawing.Size(154, 40);
+            this.OpenDestinationFolderBtn.TabIndex = 1;
+            this.OpenDestinationFolderBtn.Text = "Destination Folder";
+            this.OpenDestinationFolderBtn.UseVisualStyleBackColor = true;
+            this.OpenDestinationFolderBtn.Click += new System.EventHandler(this.OpenDestinationFolderBtn_Click);
+            // 
+            // SourceFolderLbl
+            // 
+            this.SourceFolderLbl.AutoSize = true;
+            this.SourceFolderLbl.Location = new System.Drawing.Point(176, 26);
+            this.SourceFolderLbl.Name = "SourceFolderLbl";
+            this.SourceFolderLbl.Size = new System.Drawing.Size(161, 13);
+            this.SourceFolderLbl.TabIndex = 2;
+            this.SourceFolderLbl.Text = "<- Select folder to get music from";
+            // 
+            // DestinationFolderLbl
+            // 
+            this.DestinationFolderLbl.AutoSize = true;
+            this.DestinationFolderLbl.Location = new System.Drawing.Point(176, 72);
+            this.DestinationFolderLbl.Name = "DestinationFolderLbl";
+            this.DestinationFolderLbl.Size = new System.Drawing.Size(276, 13);
+            this.DestinationFolderLbl.TabIndex = 3;
+            this.DestinationFolderLbl.Text = "<- Set destination folder, this should be the base directory";
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(13, 104);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(153, 52);
+            this.StartBtn.TabIndex = 4;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // MusicConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 521);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.DestinationFolderLbl);
+            this.Controls.Add(this.SourceFolderLbl);
+            this.Controls.Add(this.OpenDestinationFolderBtn);
+            this.Controls.Add(this.OpenSourceFolderBtn);
+            this.Name = "MusicConverter";
+            this.Text = "Music Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenSourceFolderBtn;
+        private System.Windows.Forms.Button OpenDestinationFolderBtn;
+        private System.Windows.Forms.Label SourceFolderLbl;
+        private System.Windows.Forms.Label DestinationFolderLbl;
+        private System.Windows.Forms.Button StartBtn;
+    }
+}
+
---
+++ MusicConverter/MusicConverter.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MusicConverter
+{
+    public partial class MusicConverter : Form
+    {
+        private Config config = new Config();
+        private string exeLoc;
+        BackgroundWorker worker = new BackgroundWorker();
+
+        public MusicConverter()
+        {
+            InitializeComponent();
+            
+            exeLoc = Application.ExecutablePath.Substring(0, Application.ExecutablePath.LastIndexOf(@"\"));
+
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+            worker.DoWork += new DoWorkEventHandler(Worker_DoWork);
+            worker.ProgressChanged += new ProgressChangedEventHandler(Worker_Progress);
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(Worker_Completed);
+
+            string connStr = string.Format(@"Data Source={0}\musicLibrary.SQLiteDB;", exeLoc);
+
+            using (SQLiteConnection conn = new SQLiteConnection(connStr))
+            {
+                conn.Open();
+
+                SQLiteCommand cmd = new SQLiteCommand("INSERT INTO Genre (name) VALUES ('Test')", conn);
+                SQLiteDataReader rdr = cmd.ExecuteReader();
+
+                if (rdr.HasRows)
+                {
+                    while (rdr.Read())
+                    {
+
+                    }
+                }
+            }
+        }
+
+        private void OpenSourceFolderBtn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.SelectedPath = @"D:\Development\SamplesForMusicConvertor\";
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                SourceFolderLbl.Text = fbd.SelectedPath;
+                config._srcDir = fbd.SelectedPath;
+            }
+        }
+
+        private void OpenDestinationFolderBtn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.SelectedPath = @"D:\Development\SamplesForMusicConvertor\";
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                DestinationFolderLbl.Text = fbd.SelectedPath;
+                config._destDir = fbd.SelectedPath;
+            }
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            if (config._srcDir != null && config._destDir != null)
+            {
+                if (!worker.IsBusy)
+                {
+                    worker.RunWorkerAsync(config);
+                }
+            }
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Config config = (Config)e.Argument;
+            
+ 
+            string dest = config._destDir;
+            string[] tracks = Directory.GetFiles(config._srcDir, "*.mp3", SearchOption.AllDirectories);
+            
+
+            string folderPath, trackName, destFolder, convertSrc, convertDest;
+
+            foreach (string track in tracks)
+            {
+                trackName = track.Substring(track.LastIndexOf(@"\")+1);
+                
+                folderPath = track.Substring(config._srcDir.Length);
+                folderPath = folderPath.Remove(folderPath.Length - trackName.Length);
+
+                trackName = trackName.Remove(trackName.Length - 4);
+
+                destFolder = string.Format("{0}{1}", dest, folderPath);
+                try
+                {
+                    // OGG
+                    if (!Directory.Exists(string.Format(@"{0}ogg\", destFolder)))
+                        Directory.CreateDirectory(string.Format(@"{0}ogg\", destFolder));
+
+                    // MP3
+                    if (!Directory.Exists(string.Format(@"{0}mp3\", destFolder)))
+                        Directory.CreateDirectory(string.Format(@"{0}mp3\", destFolder));
+
+                    convertSrc = string.Format(@"{0}mp3\{1}.mp3", destFolder, trackName);
+                    convertDest = string.Format(@"{0}ogg\{1}.ogg", destFolder, trackName);
+
+                    // just copy it to the MP3 folder
+                    if (track.ToLower().IndexOf(".mp3") == (track.Length - 4))
+                        File.Copy(track, convertSrc, true);
+
+                    // Convert to OGG
+                    var startInfo = new ProcessStartInfo();
+                    startInfo.FileName = string.Format(@"{0}\sox\sox.exe",exeLoc);
+                    startInfo.Arguments = string.Format("\"{0}\" \"{1}\"", convertSrc, convertDest);
+                    startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    startInfo.UseShellExecute = false;
+                    startInfo.CreateNoWindow = true;
+
+                    using (Process soxProc = Process.Start(startInfo))
+                    {
+                        soxProc.WaitForExit();
+                    }
+                    // this needs to somehow work out the artist etc
+                    JockerSoft.OggReader trackInfo = new JockerSoft.OggReader(convertDest);
+
+                }
+                catch (Exception ex)
+                {
+                    var stop = "";
+                }
+
+            }
+               
+        }
+        private void Worker_Progress(object sender, ProgressChangedEventArgs e)
+        {
+        }
+        private void Worker_Completed(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+
+        
+    }
+
+    public class Config
+    {
+        public string _srcDir = null;
+        public string _destDir = null;
+
+    }
+}
